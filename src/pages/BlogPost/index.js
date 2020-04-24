2ab338f3e2eecb0d784e260c4d55692a2a8e96ae
Lucas Barros <lbarros.comp@gmail.com>
2020-04-24 09:56:45 -0300

    Inserting the styles in post for the blog

---
+++ src/pages/BlogPost/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, PostTitle } from './styles';
+
+export default function BlogPost() {
+  return (
+    <Container>
+      <PostTitle>Blog post</PostTitle>
+    </Container>
+  );
+}
---
+++ src/pages/BlogPost/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const PostTitle = styled.p`
+    padding: 10px;
+    text-align: center;
+    font-size: 3em;
+    letter-spacing: 0.8px;
+    font-weight: 300;
+`;
